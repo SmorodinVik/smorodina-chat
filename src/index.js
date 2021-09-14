6f2a023a22bd4163a7b3485641154d02e4c12761
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-09-14 18:34:54 +0300

    test

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import '../assets/application.scss';
 // import ReactDOM from 'react-dom';
 // import React from 'react';
 
-import app from './App.jsx';
+// import app from './App.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-app();
+// app();
 
-/* const p = document.createElement('p');
+const p = document.createElement('p');
 p.classList.add('card-text');
 p.textContent = 'It works!';
 
@@ -30,7 +30,7 @@ cardBody.append(h5, p);
 
 const card = document.createElement('div');
 card.classList.add('card', 'text-center');
-card.append(cardBody); */
+card.append(cardBody);
 
 // const container = document.querySelector('#chat');
 // ReactDOM.render(
