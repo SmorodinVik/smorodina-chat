a6f94e7d599734924d47bdc426cea494d5493463
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-09-15 13:57:48 +0300

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,24 @@ import LoginPage from './Components/LoginPage.jsx';
 import ErrorPage from './Components/ErrorPage.jsx';
 
 export default () => (
-  <Router>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand as={Link} to="/">Smorodina Chat</Navbar.Brand>
-    </Navbar>
-    <div className="container-fluid h-100">
-      <Switch>
-        <Route exact path="/">
-          <LoginPage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/">
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+  <div className="d-flex flex-column h-100">
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={Link} to="/">Smorodina Chat</Navbar.Brand>
+      </Navbar>
+      <div className="container-fluid h-100">
+        <Switch>
+          <Route exact path="/">
+            <LoginPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  </div>
 );
