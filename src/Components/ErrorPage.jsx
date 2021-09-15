bdca36d0f2a4d4d8a9c3ca3a39a7a28188579b40
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-09-15 15:49:06 +0300

    update errpage

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default () => (
-  <div className="col justify-content-center align-content-center h-100">
-    <p className="text-muted">страница не найдена</p>
-    <p style={{ fontSize: '72px' }}>
+  <div className="col justify-content-center align-content-center d-flex flex-column h-100">
+    <p className="text-muted text-center">страница не найдена</p>
+    <p className="text-center" style={{ fontSize: '72px' }}>
       404
     </p>
     <a className="text-center" href="/login">на главную</a>
