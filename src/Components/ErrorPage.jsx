ddb45189a7ec72d290312e2b71e8490cebf8e54c
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-09-15 15:13:58 +0300

    up

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 export default () => (
   <div className="row justify-content-center align-content-center h-100">
+    <p className="text-muted">страница не найдена</p>
     <p style={{ fontSize: '72px' }}>
       404
     </p>
-    <p className="text-muted">страница не найдена</p>
-    <center>
-      <a className="text-center" href="/login">на главную</a>
-    </center>
+    <a className="text-center" href="/login">на главную</a>
   </div>
 );
