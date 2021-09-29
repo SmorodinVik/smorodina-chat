586d43de273141c86d0c180e47acbc961b8b6f28
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-09-29 15:24:17 +0300

    add rollbar second attemp

--- src/index.js
+++ src/index.js
@@ -11,15 +11,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store.js';
 import './i18n.js';
-import Rollbar from 'rollbar';
-
-const rollbar = new Rollbar({
-  accessToken: '67a72f81ceaa4a43935bee63a61e986e',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-});
-
-rollbar.log('Hello from Smorodina!');
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
