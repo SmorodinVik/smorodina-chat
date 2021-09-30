991a7b3fa4929c008d56fed71a4343790c032721
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-10-01 00:42:08 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import 'regenerator-runtime/runtime.js';
 
 import '../assets/application.scss';
 
+import ReactDOM from 'react-dom';
+
 import run from './init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-export default () => run();
+ReactDOM.render(run(), document.getElementById('chat'));
--- src/init.jsx
+++ src/init.jsx
@@ -1,6 +1,6 @@
 // @ts-check
 
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store.js';
