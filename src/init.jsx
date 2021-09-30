720efb2c73cccd4a1bf32555d2a0349af19ef4a7
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-10-01 00:18:53 +0300

    delete render

--- src/init.jsx
+++ src/init.jsx
@@ -1,18 +1,14 @@
 // @ts-check
 
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store.js';
 import './i18n.js';
 import App from './Components/App.jsx';
 
-export default () => {
-  console.log(document.getElementsByTagName('div'));
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('chat'),
-  );
-};
+export default () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
