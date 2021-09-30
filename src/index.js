a4a2e2ddde8809f6f72797a5e9e95253486f799f
Viktor Smorodina <smorodinaspb@yandex.ru>
2021-10-01 00:25:50 +0300

    add export from index

--- src/index.js
+++ src/index.js
@@ -11,4 +11,4 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-run();
+export default () => run();
--- src/init.jsx
+++ src/init.jsx
@@ -1,14 +1,17 @@
 // @ts-check
 
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store.js';
 import './i18n.js';
 import App from './Components/App.jsx';
 
-export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+export default () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('chat'),
+  );
+};
